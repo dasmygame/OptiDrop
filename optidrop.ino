ea3d5ddb8e5c18cdfed234c74e577ffe6fa6746c
Rahul Das <48290252+dasmygame@users.noreply.github.com>
2024-04-22 01:08:55 -0400

    Update optidrop.ino

--- optidrop.ino
+++ optidrop.ino
@@ -1,109 +1,285 @@
-#include <Adafruit_ATParser.h>
-#include <Adafruit_BLE.h>
+/*********************************************************************
+
+Embedded System software for the OptiDrop Controller (LysanDas Engineering Group - Atlanta, Georgia)
+
+*********************************************************************/
+
+#include <string.h>
+#include <Arduino.h>
 #include <SPI.h>
-#include <Adafruit_BLEBattery.h>
-#include <Adafruit_BLEEddystone.h>
-#include <Adafruit_BLEGatt.h>
-#include <Adafruit_BLEMIDI.h>
-#include <Adafruit_BluefruitLE_SPI.h>
-#include <Adafruit_BluefruitLE_UART.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
 #include "BluefruitConfig.h"
-#include <Keypad.h>
 
-const int left = 2;  // the number of the pushbutton pin
-const int right = 3;
-const int up = 4;
-const int down = 5;
-const int drop = 6;
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>
+#endif
 
-const int ledPin = 13;    // the number of the LED pin for testing
+/*=========================================================================
+    APPLICATION SETTINGS
 
-int leftState = 0;
-int rightState = 0;
-int upState = 0;
-int downState = 0;
-int dropState = 0;
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+   
+                              Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                              running this at least once is a good idea.
+   
+                              When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                              Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+       
+                              Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         1
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+/*=========================================================================*/
 
-void setup() {
-  //analog motor pins
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-  pinMode(A4, OUTPUT);
-  pinMode(A5, OUTPUT);
-  pinMode(A6, OUTPUT);
-  
-  //buttons to control track/axis movement
-  pinMode(left, INPUT);
-  pinMode(right, INPUT);
-  pinMode(up, INPUT);
-  pinMode(down, INPUT);
-  pinMode(drop, INPUT);
+// Create the bluefruit object, either software serial...uncomment these lines
 
-  Serial.begin(9600);
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+
+
+/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+// Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
+//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+// function prototypes over in packetparser.cpp
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
+float parsefloat(uint8_t *buffer);
+void printHex(const uint8_t * data, const uint32_t numBytes);
+
+// the packet buffer
+extern uint8_t packetbuffer[];
+#define sw1 2
+int sw1Bool = 0;
+int constState = 0;
+int tiltSensor = A5; 
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit App Controller Example"));
+  Serial.println(F("-----------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
+  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set Bluefruit to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("******************************"));
+  pinMode(sw1, INPUT);
 
 }
 
-void loop() {
-  delay(100);
 
-  leftState = digitalRead(left);
-  rightState = digitalRead(right);
-  
-  upState = digitalRead(up);
-  downState = digitalRead(down);
-  dropState = digitalRead(drop);
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop(void)
+{
+  // constrictor max open switch read
+  sw1Bool = digitalRead(sw1);
 
-  // check if the pushbutton is pressed. If it is, the buttonState is LOW:
-  if (leftState == LOW) {
-    Serial.println("left");
-    analogWrite(A3, 1023);
-    analogWrite(A4, 0);
+  // tilt sensor read
+  tiltSensorValue = analogRead(tiltSensor);
+
+  if (sw1Bool == 1 &&  constState == 2) {
+        analogWrite(3, 0);
+        analogWrite(4, 0);  
+        constState = 0;
   }
 
-  if (rightState == LOW) {
-    Serial.println("left");
-    analogWrite(A3, 0);
-    analogWrite(A4, 1023);
-  } 
-  
-  if (leftState == HIGH && rightState == HIGH) {
-    analogWrite(A3, 0);
-    analogWrite(A4, 0);
-  }
+  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
+  if (len == 0) return;
 
-  if (upState == LOW) {
-    // turn LED on:
-    Serial.println("left");
-    analogWrite(A1, 1023);
-    analogWrite(A2, 0);
-  
+  // Buttons
+  if (packetbuffer[1] == 'B') {
+    uint8_t buttnum = packetbuffer[2] - '0';
+    boolean pressed = packetbuffer[3] - '0';
+    Serial.print ("Button "); Serial.print(buttnum);
+    char num = (char) buttnum;
+    if (pressed) {
+      Serial.println(" pressed");
 
-  } 
+      // Close constrictor button (button "1" on number pad)
+      if (buttnum == 1) {
+        Serial.println("close");
+        analogWrite(3, 255);
+        analogWrite(4, 0);
+        constState = 1;
+      } 
+      
+      // Open constrictor button (button "2" on number pad)
+      else if (buttnum == 2 && sw1Bool == 0) {
+        Serial.println("open");
+        analogWrite(3, 0);
+        analogWrite(4, 255);
+        constState = 2;
+      } 
+      
+      //////////////////////////////////////////////////////////////////////////////////////
+      //////////////////////////////////////////////////////////////////////////////////////
+      // ADD CALIBRATION CODE WITHIN THIS CLAUSE BELOW
+      // CALIBRATION BUTTON (BUTTON "3" ON NUMBER PAD)
+      // add code within this 'else if' clause and test by pressing "3 button" on number pad
+      //
+      // controls:
+      // Pin 3 - Close constrictor motor
+      // Pin 4 - Open constrictor motor
+      // Pin 5 - Move Right motor
+      // Pin 6 - Move Left motor
+      // Pin 7 - Move Up motor
+      // Pin 8 - Move Down motor 
+      //////////////////////////////////////////////////////////////////////////////////////
+      else if (buttnum == 3) {
+        // Add code for first calibration here
+        Serial.println("Calibration A")
 
-  if (downState == LOW) {
-    // turn LED on:
-    Serial.println("left");
-    analogWrite(A1, 0);
+        // drop mode through tilt sensing
+        if (tiltSensorValue > 150) {
+          Serial.println("close");
+          analogWrite(3, 255);
+          analogWrite(4, 0);
+          constState = 1;
+        }
+      }
+      //////////////////////////////////////////////////////////////////////////////////////
+      //////////////////////////////////////////////////////////////////////////////////////
+      
+      // Left button on trackpad
+      else if (buttnum == 7) {
+        Serial.println("left");
+        analogWrite(5, 0);
+        analogWrite(6, 255);
+      } 
+      
+      // Right Button on trackpad
+      else if (buttnum == 8) {
+        Serial.println("right");
+        analogWrite(5, 255);
+        analogWrite(6, 0);
+      } 
+      
+      // Up Button on trackpad
+      else if (buttnum == 5) {
+        Serial.println("up");
+        analogWrite(7, 0);
+        analogWrite(8, 255);
+      } 
+      
+      // Down Button on trackpad
+      else if (buttnum == 6) {
+        Serial.println("down");
+        analogWrite(7, 255);
+        analogWrite(8, 0);
+      }
+
+    } 
     
-    analogWrite(A2, 1023);
-    
-  } 
+    // When no buttons are being pressed, do not move motors at all
+    else {
+      Serial.println("No Buttons pressed");
+      analogWrite(3, 0);
+      analogWrite(4, 0);
+      analogWrite(5, 0);
+      analogWrite(6, 0);
+      analogWrite(7, 0);
+      analogWrite(8, 0);
+    }
 
-  if (downState == HIGH && upState == HIGH)
-  {
-    analogWrite(A1, 0);
-    analogWrite(A2, 0);
   }
 
-  //Drop eyedrop by closing constrictor
-  if (dropState == LOW) {
-    Serial.println("left");
-    analogWrite(A5, 1023);
-    analogWrite(A6, 0);
-    
-  } else {
-    analogWrite(A5, 0);
-    analogWrite(A6, 0);
-  }
 }
